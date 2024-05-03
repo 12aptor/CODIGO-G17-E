7d22c2531ad2499eb5f121f777c63427e59a3bf0
Paolo Soncco <paholo2054@gmail.com>
2024-05-02 22:51:58 -0500

    se agregó prisma y @prisma/client al proyecto

--- semana08/express_boilerplate/src/routes/users.router.js
+++ semana08/express_boilerplate/src/routes/users.router.js
@@ -4,3 +4,7 @@ import * as usersController from "../controllers/users.controller.js";
 export const usersRouter = Router();
 
 usersRouter.get("/all", usersController.getAllUsers);
+usersRouter.post("/create", usersController.createUser);
+usersRouter.get("/by-id/:userId", usersController.getUserById);
+usersRouter.put("/update/:userId", usersController.updateUser);
+usersRouter.delete("/update/:userId", usersController.deleteUser);
