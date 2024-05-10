d0d860875e7bc38bfe84d69f504cdfcd3f16c7b1
Paolo Soncco <paholo2054@gmail.com>
2024-05-09 22:14:39 -0500

    creacion de proyecto express con typescript

--- semana09/express_ecommerce/src/controllers/auth.controller.ts
+++ semana09/express_ecommerce/src/controllers/auth.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { prisma } from "../config/prisma";
-import { CreateUserSchema } from "../schemas/users.schema";
+import { CreateUserSchema, LoginSchema } from "../schemas/users.schema";
 import bcrypt from "bcrypt";
 import { IPayload, ISignupResponse, IErrorResponse } from "../types";
 import jwt from "jsonwebtoken";
+import { ZodError } from "zod";
 
 export const signup = async (
   _req: Request,
@@ -48,6 +49,72 @@ export const signup = async (
       access_token: accessToken,
     });
   } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(400).json({
+        errors: error.issues,
+      });
+    }
+
+    if (error instanceof Error) {
+      return res.status(500).json({
+        errors: error.message,
+      });
+    }
+  }
+};
+
+export const login = async (
+  _req: Request,
+  res: Response<ISignupResponse | IErrorResponse>
+) => {
+  try {
+    const { body } = _req;
+    const validatedBody = LoginSchema.parse(body);
+
+    const user = await prisma.user.findFirst({
+      where: {
+        email: validatedBody.email,
+      },
+    });
+
+    if (!user) {
+      throw new Error("Credentials are invalid");
+    }
+
+    const isPasswordValid = bcrypt.compare(
+      validatedBody.password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      throw new Error("Credentials are invalid");
+    }
+
+    const payload: IPayload = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    };
+
+    const secretKey = process.env.SECRET_KEY;
+
+    if (!secretKey) {
+      throw new Error("Secret key not found");
+    }
+
+    const accessToken = jwt.sign(payload, secretKey, { expiresIn: "7d" });
+
+    return res.status(200).json({
+      access_token: accessToken,
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(400).json({
+        errors: error.issues,
+      });
+    }
+
     if (error instanceof Error) {
       return res.status(500).json({
         errors: error.message,
--- semana09/express_ecommerce/src/index.ts
+++ semana09/express_ecommerce/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import { usersRouter } from "./routes/users.router";
+import { authRouter } from "./routes/auth.router";
 
 const app = express();
 app.use(express.json());
 app.use(express.json());
 const port = 3000;
 
-
-
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Server is running: http://localhost:${port}`);
