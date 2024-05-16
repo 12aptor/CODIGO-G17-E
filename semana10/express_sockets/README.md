cffef43b501d430996c2e2720a47734661b0e2cc
Paolo Soncco <paholo2054@gmail.com>
2024-05-15 20:42:04 -0500

    sockets backend y frontend

---
+++ semana10/express_sockets/README.md
@@ -0,0 +1,16 @@
+# Sockets con Express
+
+## Objetivo
+El objetivo de este proyecto es crear un servidor de sockets con Express y Socket.io.
+
+## Creación del proyecto
+Para crear el proyecto, ejecutar el siguiente comando:
+```bash
+npm init -y
+```
+
+## Instalación
+Para instalar las dependencias del proyecto, ejecutar el siguiente comando:
+```bash
+npm install socket.io express nodemon
+```
---
+++ semana10/express_sockets/src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat", (message) => {
+    io.emit("chat", message)
+  })
+});
+
+server.listen(port, () => {
+  console.log(`Running: http://localhost:${port}`);
+});
